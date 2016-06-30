5e236257b79fdb43ae1e88be8730013851518f33
Michael Malleske <mmalleske@gmail.com>
2016-06-30 14:19:16 -0500

    Mike Malleske HWd23 revised

--- models/compliment.js
+++ models/compliment.js
@@ -5,6 +5,18 @@ var mongoose = require('mongoose');
 var complimentSchema = new mongoose.Schema({
   compliment: { type: String, required: true}
 });
-var Compliment = mongoose.model('Compliment', complimentSchema);
+
+
+complimentSchema.statics.findRandom = function(callback) {
+ this.count(function(err, count) {
+   if (err) {
+     return callback(err);
+   }
+   var rand = Math.floor(Math.random() * count);
+   this.findOne().skip(rand).exec(callback);
+ }.bind(this));
+};
+
+var Compliment = mongoose.model('emergency_compliment', complimentSchema);
 
 module.exports = Compliment;
