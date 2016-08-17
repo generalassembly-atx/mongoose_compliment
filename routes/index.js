2b57913cd0fe38d5bbc202e52f890ad6b166f346
Shirish Dali <designsbydali@gmail.com>
2016-08-17 18:02:37 -0500

    finished coding tested it and is working

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,58 @@ var router = express.Router();
 var Compliment = require('../models/compliment');
 
 function randomColor() {
-	var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
-	return colors[Math.floor(Math.random()*colors.length)];
+  var colors = ["#FFBF00", "#0080FF", "#01DF3A", "#FF0080"];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
 /* GET compliment form. */
 router.get('/compliment_form', function(req, res, next) {
-	var color = randomColor();
-	res.render('compliment_form', { title: 'WDI Emergency Compliment', color: color });
+  var color = randomColor();
+  res.render('compliment_form', {
+    title: 'WDI Emergency Compliment',
+    color: color
+  });
 });
 
 /* GET home page with queried name. */
 router.get('/(:name)?', function(req, res, next) {
-	name = req.params.name || "Friend";
-	var color = randomColor();
+  name = req.params.name || "Friend";
+  var color = randomColor();
 
-	// USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW
+  // USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW
+  Compliment.find({}, function(err, compliments) {
+		var compliment = compliments[Math.floor(Math.random() * compliments.length)]
+      res.render('index', {
+        title: 'WDI Emergency Compliment',
+        color: color,
+        name: name,
+        compliment: compliment.text
+      });
+    })
+    // var compliment = null; // this line is just here to temporarily prevent an undefined error. You can remove it once you get a real compliment from the DB.
 
-	// var compliment = null; // this line is just here to temporarily prevent an undefined error. You can remove it once you get a real compliment from the DB.
-	res.render('index', { title: 'WDI Emergency Compliment', color: color, name: name, compliment: compliment });
 });
 
 /* POST compliment. */
 router.post('/', function(req, res, next) {
-	var newCompliment = req.body.compliment;
+  var newCompliment = req.body.compliment;
 
-	// USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
-	res.redirect('/');
+  //CREATE
+  var compliment = new Compliment({
+    text: newCompliment
+  });
+  compliment.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+
+
+  // USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
+
 });
+
 
 module.exports = router;
