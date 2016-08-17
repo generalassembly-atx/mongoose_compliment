8c945d55b4a09d0b42099650e33be7b52371c86f
Shirish Dali <designsbydali@gmail.com>
2016-08-17 17:05:40 -0500

    compliment model added

--- models/compliment.js
+++ models/compliment.js
@@ -1,7 +1,13 @@
 // How can we set up the Compliment model to talk to our database?
+const mongoose = require('mongoose');
 // Look at past examples
 
-var Compliment = null;
+
+
+var Compliment = mongoose.model('Compliment', {
+  title: String,
+  completed: Boolean
+});
 
 // Make this available to our other files
 module.exports = Compliment;
