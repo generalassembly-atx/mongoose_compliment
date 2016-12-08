f497d4765057808fe71cbf000dbd68ac5da2b65e
vinh huynh <vvvhuynh@yahoo.com>
2016-12-07 22:56:31 -0600

    worked on mongoose_compliment HW

--- models/compliment.js
+++ models/compliment.js
@@ -1,7 +1,16 @@
 // How can we set up the Compliment model to talk to our database?
 // Look at past examples
 
-var Compliment = null;
+// /models/compliment.js
+var mongoose = require('mongoose');
+
+var Schema = new mongoose.Schema({
+  color: String,
+  name: String,
+  compliment: String
+});
+
+var Compliment = mongoose.model('Compliment', Schema);
 
 // Make this available to our other files
 module.exports = Compliment;
--- routes/index.js
+++ routes/index.js
@@ -19,17 +19,28 @@ router.get('/(:name)?', function(req, res, next) {
 	var color = randomColor();
 
 	// USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW
-
-	var compliment = null; // this line is just here to temporarily prevent an undefined error. You can remove it once you get a real compliment from the DB.
+	Compliment.find(req.params.name, function(err, result){
+	var compliment = result[Math.floor(Math.random()*result.length)].compliment;
 	res.render('index', { title: 'WDI Emergency Compliment', color: color, name: name, compliment: compliment });
+	})
 });
 
 /* POST compliment. */
 router.post('/', function(req, res, next) {
-	var newCompliment = req.body.compliment;
+	var color = randomColor();
+	var name = req.body.name || "Mr. Turtle";
+	var compliment = req.body.compliment;
 
-	// USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
-	res.redirect('/');
+	var newCompliment = Compliment({
+		color: color,
+		name: name,
+		compliment: compliment
+	});
+
+	newCompliment.save(function(err, result){
+		if (err) console.log(err);
+		res.redirect('/');
+	})
 });
 
 module.exports = router;
