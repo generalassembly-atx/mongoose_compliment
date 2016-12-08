0da277eecff63933459c722c4a35a59980c46802
Will Sparger <spargerw@gmail.com>
2016-12-08 08:43:26 -0600

    Mongoose Compliment HW

--- models/compliment.js
+++ models/compliment.js
@@ -1,7 +1,13 @@
 // How can we set up the Compliment model to talk to our database?
 // Look at past examples
+var mongoose = require('mongoose');
 
-var Compliment = null;
+var schema = new mongoose.Schema({
+  compliment: String
+});
 
 // Make this available to our other files
+
+var Compliment = mongoose.model('Compliment', schema);
+
 module.exports = Compliment;
