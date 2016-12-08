b244dfea4d5d414d3dfeb2edc48ad60281b5790f
Josh Foster <josh@allenb.com>
2016-12-08 08:47:06 -0600

    figured out random compliment

--- models/compliment.js
+++ models/compliment.js
@@ -1,9 +1,10 @@
 // How can we set up the Compliment model to talk to our database?
 // Look at past examples
 var mongoose = require('mongoose');
+
 var schema = new mongoose.Schema({
-  compliment: { type:String, required:true }
-})
+  compliment: {String}
+});
 var Compliment = mongoose.model('Compliment',schema);
 
 // Make this available to our other files
