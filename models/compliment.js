980e845e41e1a52b0881d4d199c3e6bfacf6fee1
Vinh Luu <vluu87@gmail.com>
2016-12-08 08:59:20 -0600

    mongoose compliment

--- models/compliment.js
+++ models/compliment.js
@@ -1,7 +1,16 @@
 // How can we set up the Compliment model to talk to our database?
 // Look at past examples
 
-var Compliment = null;
+var Compliment = require(Compliment);
 
 // Make this available to our other files
 module.exports = Compliment;
+
+
+
+
+var schema = new compliment.Schema({
+  compliment: { type: String, required: true },
+});
+
+var Emergency = mongoose.model('Emergency',schema);
